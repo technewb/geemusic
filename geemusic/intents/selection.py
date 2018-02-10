9d947057faade284d72635dbe3e997914b7bcda5
Laura <lauramneal3@gmail.com>
2018-02-10 01:30:31 -0500

    Added try and except to play_situation to fix key errors for stations and situation

--- geemusic/intents/selection.py
+++ geemusic/intents/selection.py
@@ -39,7 +39,11 @@ def play_situation():
     # Fetch the song
 
     situation = api.get_listen_now_situations()
-    return statement("Playing from %s situation" % (str(situation[0]["stations"][0]["name"])))
+
+    try:
+        return statement("Playing from %s situation" % (str(situation[0]["stations"][0]["name"])))
+    except KeyError:
+        return statement("Playing from %s situation" % (str(situation[0]["situations"][0]["title"])))
     created_station_id = api.create_station(situation[0]["stations"][0]["seed"]["curatedStationId"])
 
     tracks = api.get_station_tracks(created_station_id)
