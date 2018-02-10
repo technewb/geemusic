e5beccc8675e30e2e9c4159dc4b39b4ac76e6721
Laura <lauramneal3@gmail.com>
2018-02-10 01:19:15 -0500

    Checking if situation returns anything at all. Error with finding stations in the json

--- geemusic/intents/selection.py
+++ geemusic/intents/selection.py
@@ -39,7 +39,8 @@ def play_situation():
     # Fetch the song
 
     situation = api.get_listen_now_situations()
-    return statement("Playing from %s situation" % (situation[0]["stations"][0]["name"]))
+    return statement(situation)
+    # return statement("Playing from %s situation" % (situation[0]["stations"][0]["name"]))
     created_station_id = api.create_station(situation[0]["stations"][0]["seed"]["curatedStationId"])
 
     tracks = api.get_station_tracks(created_station_id)
